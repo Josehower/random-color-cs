2fd014f51f854b4062cecbdf6d6b87f8b0314b23
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-09 12:13:00 +0200

    Add random Color logic

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -2,6 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("random color");
+        Array values = Enum.GetValues(typeof(ConsoleColor));
+        ConsoleColor randomBar = (ConsoleColor)values.GetValue(new Random().Next(values.Length));
+        Console.WriteLine(randomBar);
+        Console.ForegroundColor = randomBar;
+
+
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("############     {0}      #############", randomBar);
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+
     }
 }
