59bcc3763da636642990bef448b240da992eac1c
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-09 12:48:35 +0200

    Add first c# commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Array values = Enum.GetValues(typeof(ConsoleColor));
+        ConsoleColor randomBar = (ConsoleColor)values.GetValue(new Random().Next(values.Length));
+        Console.WriteLine(randomBar);
+        Console.ForegroundColor = randomBar;
+
+
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("############     {0}      #############", randomBar);
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+        Console.WriteLine("####################################");
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This app was set up following the guide at https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-6-0
